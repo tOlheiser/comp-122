95db6f74e8e36e2573c28cad82523911f8018e32
tOlheiser <tolheiser@outlook.com>
2020-11-14 09:16:14 -0600

    Initialized arrays & displaying array data.

--- assignment4_olheiser/src/assignment4_olheiser/StudentGradeTest.java
+++ assignment4_olheiser/src/assignment4_olheiser/StudentGradeTest.java
@@ -10,5 +10,17 @@ package assignment4_olheiser;
  * @author Owner
  */
 public class StudentGradeTest {
-    
+    public static void main(String[] args) {
+        String[] name = {"Robin", "Jo", "Kelly", "Jaimie"};
+        int[] midtermScore = {28, 78, 92, 83};
+        int[] finalScore = {58, 75, 96, 79};
+        int[] assignmentGrade = {33, 80, 90, 83};
+        int[] finalGrade = new int[4];
+        
+        System.out.println("Name:\t\tMidterm:\tFinal:\t\tAssignment:");
+        for (int i = 0; i < 4; i++) {
+            System.out.printf("%s\t\t%d\t\t%d\t\t%d\n", name[i], midtermScore[i], finalScore[i], assignmentGrade[i]);
+        }
+        
+    }
 }
