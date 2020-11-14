7a6c4d2c92edd8ac5e178532406de48f2806caf2
tOlheiser <tolheiser@outlook.com>
2020-11-14 10:45:15 -0600

    Added data & output for two new students

--- assignment4_olheiser/src/assignment4_olheiser/StudentGradeTest.java
+++ assignment4_olheiser/src/assignment4_olheiser/StudentGradeTest.java
@@ -13,17 +13,57 @@ import TannersMethods.MyMethods;
  */
 public class StudentGradeTest {
     public static void main(String[] args) {
+        // Declare Arrays
         String[] name = {"Robin", "Jo", "Kelly", "Jaimie"};
         int[] midtermScore = {28, 78, 92, 83};
         int[] finalScore = {58, 75, 96, 79};
         int[] assignmentGrade = {33, 80, 90, 83};
         int[] finalGrade = new int[4];
-        finalGrade = MyMethods.getFinalGrades(midtermScore, finalScore, assignmentGrade);
         
+        // Calculate final grade & average
+        finalGrade = MyMethods.getFinalGrades(midtermScore, finalScore, assignmentGrade);
+        double averageGrade = MyMethods.getAverage(finalGrade);
+        
+        // Display data & average
         System.out.println("Name:\t\tMidterm:\tFinal:\t\tAssignment:\tFinal Grade:");
         for (int i = 0; i < 4; i++) {
             System.out.printf("%s\t\t%d\t\t%d\t\t%d\t\t%d\n", name[i], midtermScore[i], finalScore[i], assignmentGrade[i], finalGrade[i]);
         }
+        
+        System.out.printf("\nAverage Grade: %.1f\n\n", averageGrade);
+        
+        
+        // New Students Added
+        String[] newName = new String[6];
+        int[] newMidScore = new int[6];
+        int[] newFinScore = new int[6];
+        int[] newAssignGrade = new int[6];
+        int[] newFinalGrade = new int[6];
+        
+        // Add the first set of data to the new array
+        System.arraycopy(name, 0, newName, 0, 4); 
+        System.arraycopy(midtermScore, 0, newMidScore, 0, 4);
+        System.arraycopy(finalScore, 0, newFinScore, 0, 4);
+        System.arraycopy(assignmentGrade, 0, newAssignGrade, 0, 4);
+        System.arraycopy(finalGrade, 0, newFinalGrade, 0, 4);
+        
+        // Adding the new data to the last 2 elements of each array
+        newName[4] = "Terry";   newName[5] = "Kerry";
+        newMidScore[4] = 86;    newMidScore[5] = 71;
+        newFinScore[4] = 76;    newFinScore[5] = 75;
+        newAssignGrade[4] = 91; newAssignGrade[5] = 78;
+        
+        // Recalculate final grades & average as new students have been added
+        newFinalGrade = MyMethods.getFinalGrades(newMidScore, newFinScore, newAssignGrade);
+        averageGrade = MyMethods.getAverage(newFinalGrade);
+        
+        // Display data & average
+        System.out.println("Name:\t\tMidterm:\tFinal:\t\tAssignment:\tFinal Grade:");
+        for (int i = 0; i < newFinalGrade.length; i++) {
+            System.out.printf("%s\t\t%d\t\t%d\t\t%d\t\t%d\n", newName[i], newMidScore[i], newFinScore[i], newAssignGrade[i], newFinalGrade[i]);
+        }
+        
+        System.out.printf("\nAverage Grade: %.1f", averageGrade);
         
     }
 }
