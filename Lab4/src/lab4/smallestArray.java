7fe2b376133e51d90131d57c52060e7a529c5451
tOlheiser <tolheiser@outlook.com>
2020-11-16 11:55:22 -0600

    result of moving files to a different package

--- Lab4/src/lab4/smallestArray.java
+++ Lab4/src/lab4/smallestArray.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package assignment4_olheiser;
+package lab4;
 import TannersMethods.MyMethods;
 /**
  *
