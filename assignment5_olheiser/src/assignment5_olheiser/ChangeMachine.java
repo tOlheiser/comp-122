83aefdf6c9b1fed91a2f695e6d08b96699296b8a
tOlheiser <tolheiser@outlook.com>
2020-12-04 00:20:46 -0600

    fixed setters and wrote out class methods

--- assignment5_olheiser/src/assignment5_olheiser/ChangeMachine.java
+++ assignment5_olheiser/src/assignment5_olheiser/ChangeMachine.java
@@ -11,14 +11,15 @@ package assignment5_olheiser;
  */
 public class ChangeMachine {
     // declare instance variables
-    private int loonies;
-    private int toonies;
+    private int loonies = 0;
+    private int toonies = 0;
     private boolean status;
     
     // Declare Constructor
     public ChangeMachine(int loonies, int toonies) {
-        this.loonies = loonies;
-        this.toonies = toonies;
+        setLoonies(loonies);
+        setToonies(toonies);
+        checkStatus();
     }
     
     // Declare Getter methods
@@ -36,27 +37,29 @@ public class ChangeMachine {
     
     // Declare Setter Methods
     public void setLoonies(int loonies) {
+        // check for a negative value
         if (loonies < 0) {
-            System.out.println("You cannot have a negative number of coins!");
+            System.out.println("\nYou cannot have a negative number of coins!");
+        // add & assign
         } else {
             this.loonies += loonies;
         }
         
+        // update the status
         checkStatus();
-        
-        this.loonies = loonies;
     }
     
     public void setToonies(int toonies) {
+        // check for a negative value
         if (toonies < 0) {
-            System.out.println("You cannot have a negative number of coins!");
+            System.out.println("\nYou cannot have a negative number of coins!");
+        // add & assign
         } else {
             this.toonies += toonies;
         }
         
+        // update the status
         checkStatus();
-        
-        this.toonies = toonies;
     }
     
     private void setStatus(boolean status) {
@@ -65,11 +68,26 @@ public class ChangeMachine {
     
     // Declare Class Methods
     public void acceptMoney(int amount) {
+        // ensure you have the most current status
+        checkStatus();
         
+        // if the machine is working & a valid bill is entered, make change
+        if ( getStatus() && (amount == 5 || amount == 10 || amount == 20) ) {
+            makeChange(amount);
+        // output error messages accordingly.
+        } else if ( !getStatus() ) {
+            System.out.println("\nOut of Order! Here is your bill back.");
+        } else {
+            System.out.println("\nYou must insert a $5 or $10 or $20 bill. Try again.");
+        }
+        
+        // update status
+        checkStatus();
     }
     
     public void checkStatus() {
-        if (getToonies() < 10 || getLoonies() < 1) {
+        // if we have enough change, set to true, otherwise false.
+        if (getToonies() >= 10 && getLoonies() >= 1) {
             setStatus(true);
         } else {
             setStatus(false);
@@ -77,7 +95,27 @@ public class ChangeMachine {
     }
     
     private void makeChange(int amount) {
+        // subtract & assign corresponding to the amount entered.
+        if (amount == 5) {
+            System.out.println("\nYour change: Two toonies and one loonie.");
+            loonies -= 1;
+            toonies -= 2;
+        } else if (amount == 10) {
+            System.out.println("\nYour change: Five toonies.");
+            toonies -= 5;
+        } else if (amount == 20) {
+            System.out.println("\nYour change: Ten toonies.");
+            toonies -= 10;
+        }
+    }
+    
+    public void refillChange(int loonies, int toonies) {
+        // Add change
+        this.loonies += loonies;
+        this.toonies += toonies;
         
+        // update status
+        checkStatus();
     }
     
 } // End ChangeMachine Class
